8c9165902b8eee46c0eba9a8aa6631302ad286ed
elghannay <mohamedelghannay@gmail.com>
2020-12-03 00:47:12 +0100

    done solution for day 01

--- day-01/solution.js
+++ day-01/solution.js
@@ -1,6 +1,21 @@
-const fs = require('fs');
+let data = [];
 
-fs.readFile('input.txt', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+// fetching data from txt file
+async function getData() {
+  const response = await fetch('input.txt');
+  const text = await response.text();
+  data = text.split('\n');
+  checkSum(data);
+}
+
+// check the sum and log the multiplication if the sum is true
+function checkSum(data) {
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 1; j < data.length; j++) {
+      if (parseInt(data[i]) + parseInt(data[j]) === 2020)
+        console.log(data[i] * data[j]);
+    }
+  }
+}
+
+getData();
