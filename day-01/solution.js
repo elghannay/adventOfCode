715ea534d5bc31c6c88c7eddb0f3bdab628b9bd2
elghannay <mohamedelghannay@gmail.com>
2020-12-03 01:15:45 +0100

    adding solution for part two

--- day-01/solution.js
+++ day-01/solution.js
@@ -5,17 +5,28 @@ async function getData() {
   const response = await fetch('input.txt');
   const text = await response.text();
   data = text.split('\n');
-  checkSum(data);
+  // partOne(data);
+  partTwo(data);
 }
 
 // check the sum and log the multiplication if the sum is true
-function checkSum(data) {
+function partOne(data) {
   for (let i = 0; i < data.length; i++) {
     for (let j = 1; j < data.length; j++) {
       if (parseInt(data[i]) + parseInt(data[j]) === 2020)
-        console.log(data[i] * data[j]);
+        console.log('partOne:', data[i] * data[j]);
     }
   }
 }
 
+function partTwo(data) {
+  for (let k = 0; k < data.length; k++) {
+    for (let i = 1; i < data.length; i++) {
+      for (let j = 2; j < data.length; j++) {
+        if (parseInt(data[i]) + parseInt(data[j]) + parseInt(data[k]) === 2020)
+          console.log('partTwo:', data[i] * data[j] * data[k]);
+      }
+    }
+  }
+}
 getData();
